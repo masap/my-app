44736bce45942492a5e6766fba6f7a1403558871
Alexandre Alves <alexandre.alves@suse.com>
2024-11-21 12:09:55 +0000

    generate a valid rancher 2.8 extension

---
+++ pkg/my-app/constants.js
@@ -0,0 +1,4 @@
+export const CUSTOM_PAGE_NAME = 'dashboard';
+export const CUSTOM_PAGE_LABEL = 'my-dashboard-page';
+export const YOUR_PRODUCT_NAME = 'myProductName';
+export const BLANK_CLUSTER = '_';
--- pkg/my-app/product.ts
+++ pkg/my-app/product.ts
@@ -1,18 +1,16 @@
 import { IPlugin } from '@shell/core/types';
+import { CUSTOM_PAGE_NAME, CUSTOM_PAGE_LABEL, YOUR_PRODUCT_NAME, BLANK_CLUSTER } from './constants';
 
 export function init($plugin: IPlugin, store: any) {
-  const YOUR_PRODUCT_NAME = 'myProductName';
-  const BLANK_CLUSTER = '_';
-
-  const { product } = $plugin.DSL(store, YOUR_PRODUCT_NAME);
+  const { product, virtualType, basicType } = $plugin.DSL(store, YOUR_PRODUCT_NAME);
 
   product({
     icon: 'gear',
     inStore: 'management',
     weight: 100,
     to: {
-      name: `${YOUR_PRODUCT_NAME}-c-cluster`,
-      path: `/${YOUR_PRODUCT_NAME}/c/:cluster/dashboard`,
+      name: `${YOUR_PRODUCT_NAME}-c-cluster-${ CUSTOM_PAGE_NAME }`,
+      path: `/${YOUR_PRODUCT_NAME}/c/:cluster/${ CUSTOM_PAGE_NAME }`,
       params: {
         product: YOUR_PRODUCT_NAME,
         cluster: BLANK_CLUSTER,
@@ -20,4 +18,22 @@ export function init($plugin: IPlugin, store: any) {
       },
     },
   });
+
+  // this registers/creates the "dashboard" page
+   virtualType({
+    // @ts-ignore
+    label:    CUSTOM_PAGE_LABEL,
+    name:     CUSTOM_PAGE_NAME,
+    route:    {
+      name:   `${ YOUR_PRODUCT_NAME }-c-cluster-${ CUSTOM_PAGE_NAME }`,
+      params: {
+        product: YOUR_PRODUCT_NAME,
+        cluster: BLANK_CLUSTER,
+        pkg: YOUR_PRODUCT_NAME,
+      }
+    }
+  });
+
+  // this registers the "dashboard" page as a menu entry
+  basicType([CUSTOM_PAGE_NAME])
 }
